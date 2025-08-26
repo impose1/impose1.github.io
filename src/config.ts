3c5759e06a96eee7bec9d996c6997900159e6524
impose1 <1223852820@qq.com>
2025-08-26 17:59:06 +0800

    头png

--- src/config.ts
+++ src/config.ts
@@ -48,7 +48,7 @@ export const navBarConfig: NavBarConfig = {
 };
 
 export const profileConfig: ProfileConfig = {
-	avatar: "assets/images/avatar.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
+	avatar: "assets/images/avatar.jpg", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
 	name: "Heikki",
 	bio: "我是菜鸡，什么也不会，只会巴克什红狼炸撤离点",
 	links: [
